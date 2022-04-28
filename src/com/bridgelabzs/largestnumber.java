e2d4fe8e48c776d2f575e632f90a2652ec970e44
manishareddymuplam <manishamulam@gmail.com>
2022-04-28 23:21:49 +0530

    added:message

---
+++ src/com/bridgelabzs/largestnumber.java
@@ -0,0 +1,13 @@
+package com.bridgelabzs;
+import java.util.Arrays;
+
+public class largestnumber {
+    public static void main(String[] args) {
+        int array[] = {10, 20, 25, 63, 96, 57};
+        int size = array.length;
+        Arrays.sort(array);
+        System.out.println("sorted Array ::" + Arrays.toString(array));
+        int res = array[size - 2];
+        System.out.println("2nd largest element is ::" + res);
+    }
+}
